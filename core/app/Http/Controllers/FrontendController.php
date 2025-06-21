f5a8661251078337ec86679cc73ac7303430c833
Ashutosh <pashutosh563@email.com>
2025-06-21 14:11:00 +0530

    ok

--- core/app/Http/Controllers/FrontendController.php
+++ core/app/Http/Controllers/FrontendController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    
+     public function pages($slug)
+    {
+        $page = Page::where('tempname', $this->activeTemplate)->where('slug', $slug)->firstOrFail();
+        $pageTitle = $page->name;
+        $sections = $page->secs;
+        return view($this->activeTemplate . 'pages', compact('pageTitle', 'sections'));
+    }
+
 }
