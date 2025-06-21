56e4b27ad9e1a15ec2f31d319682636cc245ac9c
sagar <sagartyagi1024@gmail.com>
2025-06-21 15:44:48 +0530

    ok

--- core/app/Http/Controllers/FrontendController.php
+++ core/app/Http/Controllers/FrontendController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    
+    public function index()
+    {
+        return view('main.home'); // Make sure this blade file exists
+    }
+
 }
--- core/routes/web.php
+++ core/routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/clear', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(FrontendController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+});
+
