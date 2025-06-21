7694906b5cdab8610e88b313defae612cbd4ceeb
sagar <sagartyagi1024@gmail.com>
2025-06-21 15:14:11 +0530

    ok

--- core/app/Http/Controllers/FrontendController.php
+++ core/app/Http/Controllers/FrontendController.php
@@ -8,10 +8,7 @@ class FrontendController extends Controller
 {
      public function pages($slug)
     {
-        $page = Page::where('tempname', $this->activeTemplate)->where('slug', $slug)->firstOrFail();
-        $pageTitle = $page->name;
-        $sections = $page->secs;
-        return view($this->activeTemplate . 'pages', compact('pageTitle', 'sections'));
+       
     }
 
 }
