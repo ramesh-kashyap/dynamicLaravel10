5fb3c4b1e300c44906c22a26eab15db078d5e355
ashutosh <pashutosh563@gmail.com>
2025-06-25 13:23:16 +0530

    sss

--- core/resources/views/layouts/admin/footer.blade.php
+++ core/resources/views/layouts/admin/footer.blade.php
@@ -1,13 +1,13 @@
 
 
-  <script src="http://localhost/p2pexchange/assets/global/js/jquery-3.6.0.min.js"></script>
-    <script src="http://localhost/p2pexchange/assets/global/js/bootstrap.bundle.min.js"></script>
-    <script src="http://localhost/p2pexchange/assets/admin/js/vendor/bootstrap-toggle.min.js"></script>
-    <script src="http://localhost/p2pexchange/assets/admin/js/vendor/jquery.slimscroll.min.js"></script>
+  <script src="http://localhost/dynamicLaravel10/assets/global/js/jquery-3.6.0.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/global/js/bootstrap.bundle.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/vendor/bootstrap-toggle.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/vendor/jquery.slimscroll.min.js"></script>
 
 
-    <link rel="stylesheet" href="http://localhost/p2pexchange/assets/global/css/iziToast.min.css">
-<script src="http://localhost/p2pexchange/assets/global/js/iziToast.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/dynamicLaravel10/assets/global/css/iziToast.min.css">
+<script src="http://localhost/dynamicLaravel10/assets/global/js/iziToast.min.js"></script>
 
 
 
@@ -31,14 +31,14 @@
     }
 </script>
     
-         <script src="http://localhost/p2pexchange/assets/admin/js/vendor/datepicker.min.js"></script>
-    <script src="http://localhost/p2pexchange/assets/admin/js/vendor/datepicker.en.js"></script>
+         <script src="http://localhost/dynamicLaravel10/assets/admin/js/vendor/datepicker.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/vendor/datepicker.en.js"></script>
 
-    <script src="http://localhost/p2pexchange/assets/admin/js/nicEdit.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/nicEdit.js"></script>
 
-    <script src="http://localhost/p2pexchange/assets/admin/js/vendor/select2.min.js"></script>
-    <script src="http://localhost/p2pexchange/assets/admin/js/app.js"></script>
-    <script src="http://localhost/p2pexchange/assets/admin/js/cu-modal.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/vendor/select2.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/app.js"></script>
+    <script src="http://localhost/dynamicLaravel10/assets/admin/js/cu-modal.js"></script>
     <script>
         "use strict";
         bkLib.onDomLoaded(function() {
@@ -78,8 +78,8 @@
             });
         });
     </script>
-    <script src="http://localhost/p2pexchange/admin/users/assets/admin/js/vendor/apexcharts.min.js"></script>
-    <script src="http://localhost/p2pexchange/admin/users/assets/admin/js/vendor/chart.js.2.8.0.js"></script>
+    <script src="http://localhost/dynamicLaravel10/admin/users/assets/admin/js/vendor/apexcharts.min.js"></script>
+    <script src="http://localhost/dynamicLaravel10/admin/users/assets/admin/js/vendor/chart.js.2.8.0.js"></script>
     <script>
         "use strict";
 
